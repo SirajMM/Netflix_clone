d31b1ded8ed55f20cb99f1a9b77b7fa43954d12c
SirajMM <sirajmuhammed718@gmail.com>
2023-05-04 19:48:06 +0530

    Search  API call completed

--- lib/application/fastlaugh/fast_laugh_bloc.dart
+++ lib/application/fastlaugh/fast_laugh_bloc.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
-import 'package:netflix_clone/application/search/search_bloc.dart';
 import 'package:netflix_clone/domain/downloads/i_downloads_repo.dart';
 import '../../domain/downloads/models/downloads.dart';
 part 'fast_laugh_event.dart';
@@ -20,10 +19,21 @@ class FastLaughBloc extends Bloc<FastLaughEvent, FastLaughState> {
   FastLaughBloc(
     IDownloadsRepo _downloadService,
   ) : super(FastLaughState.initial()) {
-    on<Initialize>((event, emit) {
+    on<Initialize>((event, emit) async {
       // Get trending movies
+      final _result = await _downloadService.getDownloadsImage();
 
+      final _state = _result.fold((l) {
+        return const FastLaughState(
+          videoList: [],
+          isLoading: false,
+          isError: true,
+        );
+      },
+          (resp) => FastLaughState(
+              videoList: resp, isLoading: false, isError: false));
       // send to ui
+      emit(_state);
     });
   }
 }
